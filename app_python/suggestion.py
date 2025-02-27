0bfd3ac5d5152ebce92721244a4eca063f96974d
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2025-02-28 02:24:48 +0530

    updated prompt for suggestion generation

--- app_python/suggestion.py
+++ app_python/suggestion.py
@@ -13,16 +13,15 @@ os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
 os.environ['LANGCHAIN_TRACING_V2'] = "true"
 
-annual_data = pd.read_csv("dataset\Emission_data_monthly.csv")
+emission_details = pd.read_csv("dataset\Emission_data_monthly.csv")
 
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "You are an AI expert in carbon emission analysis for coal-based power plants."),
-        ("user", """Below is a summarized dataset showing annual trends of coal-based electricity generation:{annual_data}
+        ("user", """Below is a summarized dataset showing current trends of coal-based electricity generation:{emission_details}
             Based on this, provide:
-            1. Key emission trends over the years.
-            2. Possible future challenges in maintaining emission control.
-            3. Three effective strategies to reduce emissions while maintaining energy efficiency.
+            1. Possible future challenges in maintaining emission control.
+            2. Three effective strategies to reduce emissions while maintaining energy efficiency.
 
             Keep your response concise and data-driven.""")
     ]
@@ -33,4 +32,4 @@ output_parser = StrOutputParser()
 chain = prompt|llm|output_parser
 
 if __name__ == "__main__":
-    response = chain.invoke({'annual_data':annual_data})
+    response = chain.invoke({'emission_details':emission_details})
