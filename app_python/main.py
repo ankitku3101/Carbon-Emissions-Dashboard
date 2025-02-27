6495722ccbcb7c3d6c611ed9a9e3ce4b1b07b0a9
syedsadiquh <syedsadiquh@gmail.com>
2025-02-27 19:44:10 +0530

    Added: Uvicorn server with Auto-Reload mechanism

--- app_python/main.py
+++ app_python/main.py
@@ -12,4 +12,4 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
