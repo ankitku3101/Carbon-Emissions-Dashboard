2d8288d1e3750e2c362277d0211148e36b5aa684
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-02-27 19:50:13 +0530

    Mongodb Connection helper added.

---
+++ lib/mongodb.ts
@@ -0,0 +1,14 @@
+import mongoose, {ConnectOptions} from "mongoose";
+
+const connectMongo = async()=>{
+    if(mongoose.connection.readyState >= 1){
+        return ;
+    }
+
+    await mongoose.connect(process.env.MONGO_URI!,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    } as ConnectOptions)
+};
+
+export default connectMongo;
