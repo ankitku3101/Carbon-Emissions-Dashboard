4ac4ad6b7551a6de8944e1cbb8add741d7f7e6e0
ankitku3101 <ankit.ku3101@gmail.com>
2025-02-28 00:39:34 +0530

    Added Insights.tsx

---
+++ app/sections/Insights.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Insights() {
+  return (
+    <>
+        <section>
+            Data Trends    
+        </section>    
+        <section>
+            Feature Based Visualit..    
+        </section>    
+    </>
+)
+}
+
+export default Insights
