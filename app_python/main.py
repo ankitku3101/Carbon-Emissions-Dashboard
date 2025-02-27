ac48ccf56b57337dc2ac49af5b472ec854631dd1
syedsadiquh <syedsadiquh@gmail.com>
2025-02-27 19:38:10 +0530

    Added: Uvicorn Server to host the FastAPI Application

--- app_python/main.py
+++ app_python/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -10,3 +11,5 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
