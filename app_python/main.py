dc3f3a989ae83826fbb180c7abbbbd6a6f111107
syedsadiquh <syedsadiquh@gmail.com>
2025-02-28 02:22:29 +0530

    Added: NGROK global endpoint support

--- app_python/main.py
+++ app_python/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import uvicorn
+import json
+import ngrok
 
+# initializing the fastapi instance
 app = FastAPI()
+
+# Allowing all requests from any source
+allowed_origins = [
+    "*",
+]
+
+# Adding the middleware to the fastapi instance
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,  # frontend origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 def read_root():
@@ -12,4 +31,7 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
+    listener = ngrok.forward(addr=8080, domain="mole-model-drake.ngrok-free.app", authtoken_from_env = True)
+    print(listener.url())
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    ngrok.disconnect()
