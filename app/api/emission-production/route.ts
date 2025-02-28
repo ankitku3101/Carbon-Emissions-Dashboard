dccb8f77897f96ffa2bfb0ca916fa153ef410ffe
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-02-28 07:13:41 +0530

    Emission per production api end point setup.

---
+++ app/api/emission-production/route.ts
@@ -0,0 +1,51 @@
+import Coal from "@/models/Coal";
+import connectMongo from "@/lib/mongodb";
+import { NextRequest,NextResponse } from "next/server";
+
+export async function POST(request:NextRequest){
+    try {
+        await connectMongo();
+        const requestBody=await request.json();
+        const {start,end} = requestBody;
+
+        const aggregatedEmissionProductionData = await Coal.aggregate([
+            {
+                $match:{
+                    createdAt:{
+                        $gte:new Date(start),
+                        $lte:new Date(end)
+                    }
+                }
+            },
+            {
+                $lookup:{
+                    from:'coaluses',
+                    localField:'coaluses',
+                    foreignField:'_id',
+                    as:'coalusages'
+                },
+            },
+            {
+                $match: {
+                    $expr: { $gt: [{ $size: '$coalusages' }, 0] } // Only keep non-empty arrays
+                }
+            },
+            {
+                $project:{
+                    _id:1,
+                    "coalusages._id":1,
+                    "coalusages.coaltype":1,
+                    "coalusages.carbonemission":1,
+                    totalemission:1,
+                    production:1,
+                    createdAt:1
+                }
+            }
+        ])
+
+        return NextResponse.json({data:aggregatedEmissionProductionData,message:"Successful fetched"},{status:200});
+        
+    } catch (error) {
+        return NextResponse.json({error:error.message||"Something went wrong"},{status:500})
+    }
+}
